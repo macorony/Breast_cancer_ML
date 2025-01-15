8133484a33bd08950a55a036c5a0915cd5693c9e
Gang Yan <yangangmg@gmail.com>
2025-01-15 14:40:41 -0500

    knn model

--- models/knn.py
+++ models/knn.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score, roc_curve
 import matplotlib.pyplot as plt
 import seaborn as sns
